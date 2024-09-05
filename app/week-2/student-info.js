097a0882c4de676d6befe1ceb0e0967a5b3a68fc
Edmund <eyu1@ualberta.ca>
2024-09-05 15:15:00 -0600

    finish assignment 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/Link";
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Edmund Yu</h1>
+        <Link href = "https://github.com/Nolelle" >My Github Repository</Link>
+      </main>
+    );
+  }
